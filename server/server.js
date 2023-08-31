498abbd108a192dfb506638361db811df86dc67e
John <ashton.basile@gmail.com>
2023-08-31 11:51:01 +0100

    some database setup

--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
+const MongoClient = require("mongodb").MongoClient;
+const createRouter = require("./helpers/create_router");
 
 app.use(express.json());
+MongoClient.connect("mongodb://127.0.0.1:27017", { useUnifiedTopology: true })
+  .then((client) => {
+    const database = client.db("games_hub");
+    const games = database.collection("games");
+    const gamesRouter = createRouter(games);
+    app.use("/api/games", gamesRouter);
+  })
+  .catch(console.error);
 
 app.listen(9000, function () {
-	console.log(`Listening on port ${this.address().port}`);
+  console.log(`Listening on port ${this.address().port}`);
 });
